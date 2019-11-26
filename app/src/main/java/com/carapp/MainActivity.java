25c84984b7433caea85281c422a0364106f26793
sunabove <sunabove@users.noreply.github.com>
2019-11-26 09:01:58 +0900

    **

--- app/src/main/java/com/carapp/MainActivity.java
+++ app/src/main/java/com/carapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.carapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.v( "sunabove", "onResume");
+
+        startActivity(new android.content.Intent( this, com.carapp.CarActivity.class));
     }
 }
